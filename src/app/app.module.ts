9fbc039f1c6c7a9ffc29d4d8c5d2db83f22689f3
mittulmandhan <conanmittul@gmail.com>
2019-10-02 21:30:54 +0530

    changes regarding installation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 
@@ -10,7 +10,8 @@ import { LayoutComponent } from './layout/layout.component';
     LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
